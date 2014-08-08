150a32f855b6047944911150498ec06205b903b8
Javier Farinos <jafamo@gmail.com>
2014-08-08 12:31:48 +0100

    Add files listPackage/Debian or RedHat

---
+++ system/listPackageDebian.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# created 8-08-2014
+# @author jafamo
+#
+#Display list of all installed software on Debian
+
+dpkg --list
+
+
---
+++ system/listPackageRedHat.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# created 8-08-2014
+# @author jafamo
+#
+#Display list of all installed software on RedHat
+
+rpm -qa | grep 'software-name'
+
+
+

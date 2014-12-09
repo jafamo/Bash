e5ddccb423baed8c7da7ec477cb8e02f331d3cc4
jafamo <jafamo@gmail.com>
2014-12-09 08:27:53 +0000

    fix bug metadataGUI.sh test exist exiftool installed

--- scripts/metadataGUI.sh
+++ scripts/metadataGUI.sh
@@ -8,22 +8,32 @@
 # Created file info.txt with information metadata $1
 #
 
-name=$(zenity --file-selection --title="Select image")
-echo
-echo $name
-echo 
-#echo "Introduce name of image: " 
-#read name
+commando=$(which exiftool>>/tmp/comando)
+cat /tmp/comando
+ 
+if [ -s $comando ]; then
+	echo "You have the exiftool"
+else
+	echo "You need install exiftool"
+	exit 0;
+fi
+
+	name=$(zenity --file-selection --title="Select image")
+	echo
+	echo $name
+	echo 
+	#echo "Introduce name of image: " 
+	#read name
 
 case $? in 
 		0)
-			echo "\"$name\" ha sido seleccionado."
+			echo "\"$name\" It's exist."
 			exiftool -a $name > info.txt
 			echo "Created information in info.txt"
 			;;
 
 		1)
-			echo "You do not select image"
+			echo "You don't select image"
 			;;
 
 		-1)

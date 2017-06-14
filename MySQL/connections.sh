557c7d1ec82d1afee6496dda8d7c0d7a68559152
Javier <jafamo@gmail.com>
2017-06-14 10:36:20 +0200

    Add debug mysqladmin command every 1 second

--- MySQL/connections.sh
+++ MySQL/connections.sh
@@ -4,6 +4,9 @@
 # https://support.plesk.com/hc/en-us/articles/213938885-Plesk-or-website-are-not-working-Unable-to-connect-to-database-MySQL-server-has-gone-away-Full-server-backup-is-created-with-warning-errno-24-Too-many-open-files
 #
 # https://support.plesk.com/hc/es/articles/213391989
+#
+# http://www.thegeekstuff.com/2009/01/15-practical-usages-of-mysqladmin-command-for-administering-mysql-server/
+
 
 
 echo "show connections"
@@ -15,10 +18,13 @@ mysqladmin -uadmin -p`cat /etc/psa/.psa.shadow ` variables | grep 'max.*connecti
 echo "comprobar que conexiones y a que base de datos se esta realizando"
 mysqladmin -uadmin -p`cat /etc/psa/.psa.shadow` processlist
 
+echo "Debug and identify the query that is causing problems"
+mysqladmin -u root -ptmppassword -i 1 processlist
+
 
 echo
 echo"0 max_user_connections are unlimited."
-echo "you need to edit /etc/mysql/my.cnf"
+echo "you need to edit /etc/mysql/my.cnf   OR  /etc/my.conf"
 echo
 echo" restart the service... "
 echo " You need to execute this:  service mysqld restart "
